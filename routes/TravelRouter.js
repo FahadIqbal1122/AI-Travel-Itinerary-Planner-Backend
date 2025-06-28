4aece4d97edc675e9cc80ab1713e3e3d30b452eb
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2025-06-28 19:27:10 +0300

    Authorization now working

--- routes/TravelRouter.js
+++ routes/TravelRouter.js
@@ -1,9 +1,25 @@
 const router = require("express").Router()
 const controller = require("../controllers/TravelController")
+const middleware = require("../middleware")
 
 router.get("/", controller.GetTravels)
-router.post("/", controller.CreateTravel)
-router.put("/:travel_id", controller.UpdateTravel)
-router.delete("/:travel_id", controller.DeleteTravel)
+router.post(
+  "/",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateTravel
+)
+router.put(
+  "/:travel_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateTravel
+)
+router.delete(
+  "/:travel_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteTravel
+)
 
 module.exports = router
